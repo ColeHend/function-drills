209da5759c2e5239af8b69c9dac66b9758e21d97
Cole Henderson <darkgale93@gmail.com>
2022-01-24 16:47:57 -0700

    fixing problem 21

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -330,12 +330,10 @@ let sampleArray = [0,1,2,3,4,7,5,6,8,9]
 */
 
 function ascending(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (i < arr.length-1){
-      if (arr[i]+1 !== arr[i+1]) {
+  for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i-1]) {
           return false
       } 
-    }
   }
   return true
 }
